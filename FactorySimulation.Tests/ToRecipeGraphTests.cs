97178c664ae090bc6da9859ae25894781ffbb2f1
Vlad <pikova98@bk.ru>
2024-02-14 17:45:01 +0500

    simple test for toRecipieGraph method

---
+++ FactorySimulation.Tests/ToRecipeGraphTests.cs
@@ -0,0 +1,27 @@
+namespace FactorySimulation.Tests;
+
+public class ToRecipeGraphTests
+{
+    [Fact]
+    public void ToRecipeGraph_Works(){
+        var recipes = ResourceTransformerInfo.ManyFromJson(File.ReadAllText("rep.json"));
+        var nameToRecipe = recipes.ToDictionary(r=>r.OutputResources.First().resourceName,r=>r);
+        var graph = recipes.ToRecipeGraph(nameToRecipe["result"],1);
+        var graphRecipes = graph.Nodes.Select(n=>n.Recipe).OrderBy(r=>r.GetHashCode());
+        Assert.Equal(recipes.OrderBy(r=>r.GetHashCode()),graphRecipes);
+
+        foreach(var e in graph.Edges){
+            var source = graph.Nodes[e.SourceId].Recipe;
+            var target = graph.Nodes[e.TargetId].Recipe;
+            var edgeRes = 
+                target.OutputResources
+                .Select(r=>r.resourceName)
+                .Intersect(
+                    source.InputResources
+                    .Select(r=>r.resourceName)
+                ).First();
+            Assert.Equal(edgeRes,e.Resource);
+        }
+
+    }
+}
